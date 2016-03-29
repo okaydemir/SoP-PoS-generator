f7e8aa61fc238025332d49306cb416758e894922
okaydemir <okay-demir@hotmail.com>
2016-03-29 07:01:28 +0300

    readability&comments

--- possop.cpp
+++ possop.cpp
@@ -13,35 +13,38 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-    int N,K;
-    vector<string> s;
-    string x;
-    vector<vector <bool> > table;
-    bool sop;
+    int N,K; // N = number of variables, K=number of rows of truth table
+    vector<string> s; // variable names vector    
+    vector<vector <bool> > table; // truth table
     string sop_f="";
     string pos_f="";
 
-    int y;
+
 ifstream myfile (argv[1]);
     if (myfile.is_open())
     {
+
+
+        //reading first line that has #ofvar,varnames,#of rows of truth table
     	myfile >> N;
     	for(int i=0;i<N;i++){
-    		myfile >> x;
-    		s.push_back(x);
+            string temp;
+    		myfile >> temp;
+    		s.push_back(temp);
     	}
     	myfile >> K;
 
     	
+
+        //reading truth table and storing it
     	for(int i=0;i<K;i++){
     		  vector <bool> temp;
     		for(int i=0;i<N+1;i++){
-    		myfile >> y;
-    		//cout << y << " ";
-    		temp.push_back(y);
+                bool intemp;
+    		myfile >> intemp;
+    		temp.push_back(intemp);
 
     	}
-    	//cout << endl;
 
     	table.push_back(temp);
 
@@ -49,13 +52,15 @@ ifstream myfile (argv[1]);
 
     }
     else cout << "Unable to open file\n";
-//cout << table[0][N];
+
+
+    bool sop; //is current row related with SoP
 
    for(size_t i=0;i<table.size();i++){
 
-    	if(table[i][N]){
-    		sop=true;
-    	}else{sop=false;}
+    	if(table[i][N]){ //for each row if output is 1
+    		sop=true; // we shall use that row for SoP
+    	}else{sop=false;} // else for PoS
 
 if(sop){
 
@@ -63,22 +68,22 @@ if(sop){
     		for(size_t j=0;j<N;j++){
 
     			if (table[i][j])
-    			sop_f+= s[j];
+    			sop_f+= s[j]; // if variable's value is 1 add it to outstring directly
     		else
-    			sop_f+= s[j] + "'";
+    			sop_f+= s[j] + "'"; // if 0 add complement
     		}
-    		sop_f+= "+";
-    		}else{
-pos_f+="(";
+    		sop_f+= "+"; // put plus sign after a column is processed and a single product is added to the SoP string
+    		}else{ // if PoS
+pos_f+="("; // Product of Sums therefore parentheses
 for(size_t j=0;j<N;j++){
 
     			if (table[i][j])
-    			pos_f+= s[j] + "'" + "+";
+    			pos_f+= s[j] + "'" + "+"; //reverse situation of SoP
     		else
     			pos_f+= s[j] + "+";
 
     	}
-pos_f.erase(pos_f.end()-1);
+pos_f.erase(pos_f.end()-1); // remove the plus sign after last element
 pos_f+=")";
 
 
